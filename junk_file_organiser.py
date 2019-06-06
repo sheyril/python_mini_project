eb0b2f0d04495da774b0764ba8ec8c756f5907bd
sheyril <40118763+sheyril@users.noreply.github.com>
2019-06-06 19:50:35 +0530

    Update junk_file_organiser.py

--- junk_file_organiser.py
+++ junk_file_organiser.py
@@ -2,7 +2,13 @@ import os
 import shutil
 
 # print (os.getcwd())
+
 given_dir = input('Enter the directory path: ')
+
+while not os.path.exists(given_dir):
+    print ('Please enter a valid path ')
+    given_dir = input('Enter the directory path: ')
+
 
 # to set cwd to the given dir
 if os.getcwd() != given_dir:
@@ -10,103 +16,49 @@ if os.getcwd() != given_dir:
     print('changing directory to given directory')
     os.chdir(given_dir)
 
-'''# to set cwd to the desktop
-if os.getcwd() != '/Users/sheyril/Desktop':
-    # print(os.getcwd())
-    print('changing directory to desktop')
-    os.chdir('/Users/sheyril/Desktop')
-'''
 
+# to create the main folder
+try:
+    os.mkdir('Files')
+except FileExistsError:
+    print('File already exists')
 
 # creating folders
 
-def create_folders():
 
-    # to create the main folder
-    try:
-        os.mkdir('Files')
-    except FileExistsError:
-        print('File already exists')
+def create_folders(folder):
 
     # to create the sub folders:
     os.chdir(given_dir + '/Files')
     # print(os.getcwd())
 
     try:
-        os.mkdir('Audio')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Pictures')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Videos')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Documents')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Compressed')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Miscellaneous')
-    except FileExistsError:
-        print('File already exists')
-    try:
-        os.mkdir('Installation')
+        os.mkdir(folder)
     except FileExistsError:
         print('File already exists')
 
 
-create_folders()
+ext_dict = {}
+
+folders_dict = {'Audio': ['.mp3'], 'Documents': ['.pdf', '.txt', '.xlsx'],
+                'Videos': ['.mp4', '.mkv'], 'Compressed': ['.zip'],
+                'Installation': ['.pkg', 'exe', 'dmg'], 'Miscellaneous': []}
+
+for folder, extentions in folders_dict.items():
+
+    create_folders(folder)
+    # print (folder, extentions)
+    for extention in extentions:
+        ext_dict[extention] = folder
+# print (ext_dict)
 
 
 def segregate(file, name, extention):
     # print (name)
-    if extention in ['.txt', '.xlsx', '.pdf']:
+    if extention in ext_dict:
         try:
             shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Documents'))
-        except FileExistsError:
-            print('File already exists')
-    elif extention in ['.jpg', '.png']:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Pictures'))
-        except FileExistsError:
-            print('File already exists')
-    elif extention in ['.zip']:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Compressed'))
-        except FileExistsError:
-            print('File already exists')
-    elif extention in ['.pkg', '.exe', '.dmg']:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Installation'))
-        except FileExistsError:
-            print('File already exists')
-    elif extention in ['.mkv', 'mp4']:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Videos'))
-        except FileExistsError:
-            print('File already exists')
-    elif extention in ['.mp3']:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Audio'))
-        except FileExistsError:
-            print('File already exists')
-    else:
-        try:
-            shutil.move(os.path.join(given_dir, file),
-                        os.path.join(given_dir, 'Files/Miscellaneous'))
+                        os.path.join(given_dir, 'Files/' + ext_dict[extention]))
         except FileExistsError:
             print('File already exists')
 
@@ -115,7 +67,7 @@ def segregate(file, name, extention):
 files_list = os.listdir(given_dir)
 files_list.remove('.DS_Store')
 files_list.remove('Files')
-#print(files_list)
+# print(files_list)
 
 for file in files_list:
     name, extention = os.path.splitext(file)
